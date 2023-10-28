da36ea54fb7ea604b3f639a8393d52535939b20d
신희성 <vvwv3092@gmail.com>
2023-10-29 01:16:35 +0900

    init

---
+++ src/main/java/com/example/tmboard/TmBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.tmboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TmBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tmboard/TmBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tmboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
